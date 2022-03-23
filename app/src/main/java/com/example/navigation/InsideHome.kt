e09f1c333ed5f9e45bd8147851969b87e766d4f8
victo <vicky454ph@gmail.com>
2022-03-23 01:17:44 -0600

    recibimos argumento en el fragment

--- app/src/main/java/com/example/navigation/InsideHome.kt
+++ app/src/main/java/com/example/navigation/InsideHome.kt
@@ -5,20 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.navigation.databinding.ActivityMainBinding
+import com.example.navigation.databinding.FragmentInsideHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class InsideHome : Fragment() {
 
-
+    private lateinit var  binding: FragmentInsideHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         //recibimos los parametro
+        binding = FragmentInsideHomeBinding.inflate(layoutInflater)
+                                            //recibimos string porque asi le agregamos el argumento de tipo string en nav_graph
         val argumentoRecibido = arguments?.getString("argumentoPasado")
-        return inflater.inflate(R.layout.fragment_inside_home, container, false)
+        binding.textView2.setText(argumentoRecibido)
+        return  binding.root
+
     }
 }
